86ed43a965262c9f2afeccf1b116263ccccebed0
Sofi Dobriak <sofi.dobriak@gmail.com>
2026-05-14 12:25:39 +0300

    remove cors

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const cors = require('cors');
 const morgan = require('morgan');
 const { generateId } = require('./helpers/helpers');
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
 app.use(express.static('dist'));
 
 morgan.token('body', req => JSON.stringify(req.body));
