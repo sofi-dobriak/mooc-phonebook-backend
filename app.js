51f735ea475cf31a2eaa50eaa8240dc118f27f7b
Sofi Dobriak <sofi.dobriak@gmail.com>
2026-05-17 12:43:07 +0300

    add code for backend - blog list app

--- app.js
+++ app.js
@@ -1,33 +1,16 @@
-const mongoose = require('mongoose');
 const express = require('express');
-const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoute = require('./controllers/blogList.js');
+const middleware = require('./utils/middleware.js');
 const config = require('./utils/config');
-const logger = require('./utils/logger');
-const contactsRoute = require('./controllers/contacts');
-const infoRoute = require('./controllers/info');
-const middleware = require('./utils/middleware');
 
 const app = express();
 
-logger.info('connecting to', config.URL);
-
-mongoose
-  .connect(config.URL, { family: 4 })
-  .then(() => {
-    logger.info('connected to MongoDB');
-  })
-  .catch(error => {
-    logger.error('error connecting to MongoDB:', error.message);
-  });
+mongoose.connect(config.URL, { family: 4 });
 
 app.use(express.json());
-app.use(express.static('dist'));
 
-morgan.token('body', req => JSON.stringify(req.body));
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-
-app.use('/api/contacts', contactsRoute);
-app.use('/api/info', infoRoute);
+app.use('/api/blogs', blogRoute);
 
 app.use(middleware.errorHandler);
 app.use(middleware.unknownEndpoint);
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const app = require('./app');
+const app = require('./app.js');
 const config = require('./utils/config');
-const logger = require('./utils/logger');
+const logger = require('./utils/logger.js');
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`);
